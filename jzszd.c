6e824742c8a3b4cedbaf9eacea21641f88e8bbf3
unknown <1607406195@qq.com>
2019-11-16 11:49:10 +0800

    cmy

---
+++ jzszd.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+double fact(int n);
+int main()
+{
+    int i;
+    double sum;
+    sum=0;
+    for(i=1;i<=100;i++)
+    sum=sum+fact(i);
+    printf("1£¡+2£¡+...+100!=%e\n",sum);
+    return 0;
+}
+double fact(int n)
+{
+    int i;
+    double result;
+    result=1;
+    for(i=1;i<=n;i++)
+    result=result*i;
+    return result;
+}
+
+
+    
